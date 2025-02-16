f5869dba2fb2d045d19dd57dd2cc86c94b55f37c
Deepinder Singh Brar <deepindersinghdeep03@gmail.com>
2025-02-16 12:41:59 +0530

    Delete Learning SQL/01-Data Retrieval directory

--- Learning SQL/01-Data Retrieval/01-select.sql
+++
@@ -1,7 +0,0 @@
-use employees;
--- select * from employees.employees;
-select * from employees.employees;
-
-select name,age from employees;
-
-select name as dihadie, age as expiry_date from employees;
--- Learning SQL/01-Data Retrieval/02-selecting_ages_above_30.sql
+++
@@ -1,4 +0,0 @@
-use employees;
-
-select name,age from employees where age > 30; 
--- selects only those whose ages are above 30
